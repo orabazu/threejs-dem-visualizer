b73ffec623a7df232f074f127c9923b330960e1a
Orhun Özer <ozerorhun@gmail.com>
2021-07-26 01:45:12 +0300

    zhunor/update-webpack-and-deps (#3)
    * Update dependencies
    - webpack
    - babel
    - eslint
    - prettier
    
    * Fix linter issues
    
    * Update package.json
    
    * Add clean webpack
    
    * remove logs
    
    * Update readme
    
    * Reduce bundle size

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,33 @@
-import * as THREE from "three";
+import {
+  Scene,
+  PerspectiveCamera,
+  DirectionalLight,
+  WebGLRenderer,
+  PlaneGeometry,
+  GridHelper,
+  AxesHelper,
+  TextureLoader,
+  MeshLambertMaterial,
+  DoubleSide,
+  Mesh,
+} from 'three/build/three.min.js'; // import min because three.js is not tree-shakable for now
 // TODO: OrbitControls import three.js on its own, so the webpack bundle includes three.js twice!
-import OrbitControls from "orbit-controls-es6";
-import * as Detector from "../js/vendor/Detector";
+import OrbitControls from 'orbit-controls-es6';
+import * as Detector from '../js/vendor/Detector';
 
 // TODO: Major performance problems on reading big images
 // import * as terrain from "../textures/agri-medium-dem.tif";
 // import * as mountainImage from "../textures/agri-medium-autumn.jpg";
 
-import * as terrain from "../textures/agri-small-dem.tif";
-import * as mountainImage from "../textures/agri-small-autumn.jpg";
-import * as GeoTIFF from "geotiff";
+import terrain from '../textures/agri-small-dem.tif';
+import mountainImage from '../textures/agri-small-autumn.jpg';
 
-require("../sass/home.sass");
+require('../sass/home.sass');
 
 class Application {
   constructor(opts = {}) {
     this.width = window.innerWidth;
-    this.height = window.innerHeight
+    this.height = window.innerHeight;
 
     if (opts.container) {
       this.container = opts.container;
@@ -31,14 +42,14 @@ class Application {
       this.render();
     } else {
       // TODO: style warning message
-      console.log("WebGL NOT supported in your browser!");
+      console.log('WebGL NOT supported in your browser!');
       const warning = Detector.getWebGLErrorMessage();
       this.container.appendChild(warning);
     }
   }
 
   init() {
-    this.scene = new THREE.Scene();
+    this.scene = new Scene();
     this.setupRenderer();
     this.setupCamera();
     this.setupControls();
@@ -46,7 +57,7 @@ class Application {
     this.setupTerrainModel();
     this.setupHelpers();
 
-    window.addEventListener("resize", () => {
+    window.addEventListener('resize', () => {
       const w = window.innerWidth;
       const h = window.innerHeight;
       this.renderer.setSize(w, h);
@@ -65,16 +76,14 @@ class Application {
   }
 
   static createContainer() {
-    const div = document.createElement("div");
-    div.setAttribute("id", "canvas-container");
-    div.setAttribute("class", "container");
-    // div.setAttribute('width', window.innerWidth);
-    // div.setAttribute('height', window.innerHeight);
+    const div = document.createElement('div');
+    div.setAttribute('id', 'canvas-container');
+    div.setAttribute('class', 'container');
     return div;
   }
 
   setupRenderer() {
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer = new WebGLRenderer({ antialias: true });
     this.renderer.setClearColor(0xd3d3d3); // it's a dark gray
     this.renderer.setPixelRatio(window.devicePixelRatio || 1);
     this.renderer.setSize(this.width, this.height);
@@ -87,7 +96,7 @@ class Application {
     const aspect = this.width / this.height;
     const near = 0.1;
     const far = 10000;
-    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera = new PerspectiveCamera(fov, aspect, near, far);
     this.camera.position.set(1000, 1000, 1000);
     this.camera.lookAt(this.scene.position);
   }
@@ -101,10 +110,10 @@ class Application {
   }
 
   setupLight() {
-    this.light = new THREE.DirectionalLight(0xffffff);
+    this.light = new DirectionalLight(0xffffff);
     this.light.position.set(500, 1000, 250);
     this.scene.add(this.light);
-    // this.scene.add(new THREE.AmbientLight(0xeeeeee));
+    // this.scene.add(new AmbientLight(0xeeeeee));
   }
 
   setupTerrainModel() {
@@ -113,74 +122,65 @@ class Application {
       const tifImage = await rawTiff.getImage();
       const image = {
         width: tifImage.getWidth(),
-        height: tifImage.getHeight()
+        height: tifImage.getHeight(),
       };
-      
 
       /* 
       The third and fourth parameter are image segments and we are subtracting one from each,
        otherwise our 3D model goes crazy.
        https://github.com/mrdoob/three.js/blob/master/src/geometries/PlaneGeometry.js#L57
        */
-      const geometry = new THREE.PlaneGeometry(
+      const geometry = new PlaneGeometry(
         image.width,
         image.height,
         image.width - 1,
-        image.height -1
+        image.height - 1
       );
       const data = await tifImage.readRasters({ interleave: true });
 
-      console.time("parseGeom");
+      console.time('parseGeom');
       geometry.vertices.forEach((geom, index) => {
         geom.z = (data[index] / 20) * -1;
       });
-      console.timeEnd("parseGeom");
+      console.timeEnd('parseGeom');
 
-      const texture = new THREE.TextureLoader().load(mountainImage);
-      const material = new THREE.MeshLambertMaterial({
+      const texture = new TextureLoader().load(mountainImage);
+      const material = new MeshLambertMaterial({
         wireframe: false,
-        side: THREE.DoubleSide,
-        map: texture
+        side: DoubleSide,
+        map: texture,
       });
 
-      const mountain = new THREE.Mesh(geometry, material);
+      const mountain = new Mesh(geometry, material);
       mountain.position.y = 0;
       mountain.rotation.x = Math.PI / 2;
 
       this.scene.add(mountain);
 
-      const loader = document.getElementById("loader");
-      loader.style.opacity = "-1";
+      const loader = document.getElementById('loader');
+      loader.style.opacity = '-1';
 
       // After a proper animation on opacity, hide element to make canvas clickable again
-      setTimeout(
-        (() => {
-          loader.style.display = "none";
-        }),
-        1500
-      );
+      setTimeout(() => {
+        loader.style.display = 'huhuhu';
+      }, 1500);
     };
 
     readGeoTif();
   }
 
   setupHelpers() {
-    const gridHelper = new THREE.GridHelper(1000, 40);
+    const gridHelper = new GridHelper(1000, 40);
     this.scene.add(gridHelper);
-
-    // const dirLightHelper = new THREE.DirectionalLightHelper(this.light, 10);
-    // this.scene.add(dirLightHelper);
-
-    console.log("The X axis is red. The Y axis is green. The Z axis is blue.");
-    const axesHelper = new THREE.AxesHelper(500);
+    console.log('The X axis is red. The Y axis is green. The Z axis is blue.');
+    const axesHelper = new AxesHelper(500);
     this.scene.add(axesHelper);
   }
 }
 
-// wrap everything inside a function scope and invoke it (IIFE, a.k.a. SEAF)
 (() => {
   const app = new Application({
-    container: document.getElementById("canvas-container")
+    container: document.getElementById('canvas-container'),
   });
   console.log(app);
 })();
