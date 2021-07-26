e4b77de68ce23f4924b02f63874afd18cef40815
Orhun Özer <ozerorhun@gmail.com>
2021-07-26 13:21:10 +0300

    Zhunor/update webpack and deps (#4)
    * Update dependencies
    - webpack
    - babel
    - eslint
    - prettier
    
    * Fix linter issues
    
    * Update package.json
    
    * Add clean webpack
    
    * remove logs
    
    * Update readme
    
    * Reduce bundle size
    
    * Update webpack config
    
    * Remove compress

---
+++ webpack.common.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+
+module.exports = {
+  target: "web",
+  context: __dirname,
+  entry: {
+    home: ["/src/js/index.js", "/src/sass/home.sass"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[chunkhash].js",
+    chunkFilename: "[id].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, "js", "src")],
+        exclude: [path.resolve(__dirname, "node_modules")],
+        use: {
+          loader: "eslint-loader"
+        }
+      },
+      {
+        test: /\.(css|sass|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              //   modules: true,
+              // importLoaders allows to configure how many loaders before css-loader should be applied to @imported resources.
+              // 0 => no loaders (default); 1 => postcss-loader; 2 => postcss-loader, sass-loader
+              importLoaders: 2,
+            }
+          },
+          {
+            loader: "sass-loader",
+          }
+        ]
+      },
+      // rule for textures (images)
+      {
+        test: /\.(jpe?g|png|tif)$/i,
+        include: path.resolve(__dirname, "src", "textures"),
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              disable: true, // webpack@2.x and newer
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                optimizationLevel: 7,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    contentBase: path.join(__dirname, "build"),
+    inline: true, // live reloading
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: false,
+      modules: false
+    }
+  },
+
+  plugins: [
+    // new BundleAnalyzerPlugin(),
+    new CleanWebpackPlugin({
+      root: __dirname,
+      exclude: ["favicon.ico"],
+      verbose: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "templates", "index.html"),
+      hash: true,
+      filename: "index.html",
+      chunks: ["commons", "home"]
+    }),
+  ],
+
+  externals: {
+    GeoTIFF: 'GeoTIFF',
+  },
+};
